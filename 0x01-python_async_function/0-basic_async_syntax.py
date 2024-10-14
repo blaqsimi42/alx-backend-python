e6e3bb4c7283dc29bf90d8aebc331103465c1d96
blaqsimi42 <cimee464@gmail.com>
2024-10-14 14:47:26 +0000

    0x01-python_async_function

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,22 +1,17 @@
 #!/usr/bin/env python3
-
 """
-basics of 
-using the asyncio library in python
+A module for asynchronous tasks using asyncio.
 """
 
-import random
 import asyncio
+import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
     """
-    couroutine waits for a random number of seconds
-C
-    and returns the total number of seconds waited
+    Wait for a random amount of time up to the max_delay seconds.
+    and returns the amount of time waited.
     """
-    if not isinstance(max_delay, int):
-        raise TypeError('Max wait should be an int')
     wait_time = random.random() * max_delay
     await asyncio.sleep(wait_time)
     return wait_time
